e991adf39c178d1fd0ea3c7a5646ebb353cb2edf
Muhammad Usman Shaukat <usmanshaukat3737@gmail.com>
2025-12-05 00:31:46 +0500

    bhai

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve("./.env") }); // relative to current working dir
+//dotenv.config(); // loads .env in the same folder
 import mongoose from "mongoose";
-console.log("Mongo URL:", process.env.MONGO_URL);
+
+console.log("Mongo URL:", process.env.MONGO_URL); // now correct
+
+
 export const connectDB = async () => {
   try {
     const db = process.env.MONGO_URL;
